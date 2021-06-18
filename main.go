5242aea8d0f2cc53c9529a4c1eadb8af119afe1d
aizwellenstan <aizwellenstan@gmail.com>
2021-06-18 19:09:04 +0900

    concurrency

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	"fmt"
+	"time"
+)
+
+func main(){
+	out1 := make(chan string)
+	out2 := make(chan string)
+
+	go func(){
+		for{
+			time.Sleep(time.Second /2)
+			out1 <- "order processed"
+		}
+	}()
+	go func(){
+		for{
+			time.Sleep(time.Second)
+			out2 <- "refund processed"
+		}
+	}()
+	
+	for{
+		select{
+			case msg := <- out1:
+				fmt.Println(msg)
+			case msg := <- out2:
+				fmt.Println(msg)
+		}
+	}
+}
